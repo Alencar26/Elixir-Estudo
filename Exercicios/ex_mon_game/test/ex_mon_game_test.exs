ae3296b50c5502dfe77a1026aef7e138b35090af
Andre Alencar <aaugusto310@gmail.com>
2020-12-29 11:00:15 -0300

    iniciado testes unitários no módulo principlal ExMonGame, feito importe do CaptureIO

--- Exercicios/ex_mon_game/test/ex_mon_game_test.exs
+++ Exercicios/ex_mon_game/test/ex_mon_game_test.exs
@@ -1,7 +1,36 @@
 defmodule ExMonGameTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert true == true
+  import ExUnit.CaptureIO
+
+  alias ExMonGame.Player
+
+
+  describe "create_player/4" do
+    test "return a player" do
+      expected_response = %Player{
+        life: 100,
+        moves: %{move_avg: :chute, move_heal: :cura, move_rnd: :soco},
+        name: "player"
+      }
+
+      assert expected_response == ExMonGame.create_player("player", :chute, :soco, :cura)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+
+      player = Player.build("player", :chute, :soco, :cura)
+
+      messages =
+        capture_io(fn ->
+          assert ExMonGame.start_game(player) == :ok
+      end)
+
+      assert messages =~ "The game is started!"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
   end
 end
